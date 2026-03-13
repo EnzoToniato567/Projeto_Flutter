b48d284351c59d1c36ed405ada07e9fbc8f27ea2
EnzoToniato567 <enzotoniato0303@gmail.com>
2026-03-13 07:36:16 -0300

    Projeto flutter

--- formularios/lib/main.dart
+++ formularios/lib/main.dart
@@ -1,60 +1,41 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MaterialApp(title: 'IMC', home: App()));
 }
 
-class MainApp extends StatefulWidget {
-  const MainApp({super.key});
+class App extends StatefulWidget {
+  const App({super.key});
 
   @override
-  State<MainApp> createState() => _MainAppState();
+  State<App> createState() => _AppState();
 }
 
-class _MainAppState extends State<MainApp> {
-  String nome = '';
-  String resultado = 'Resultado';
+class _AppState extends State<App> {
+  String? nome;
   double peso = 0.0;
-  double imc = 0.0;
   double altura = 0.0;
+  double imc = 0.0;
+  String? resultado;
 
-  void processar() {
-    setState(() {
-      imc = peso / (altura * altura);
+  void calcular() {
+    imc = peso / (altura * altura);
+    resultado = "$nome seu IMC é ${imc.toStringAsFixed(1)}";
+  }
 
-      if (imc < 18.5) {
-        resultado =
-            "$nome, seu IMC é ${imc.toStringAsFixed(1)}. Você está abaixo do peso.";
-      } else if (imc <= 24.9) {
-        resultado =
-            "$nome, seu IMC é ${imc.toStringAsFixed(2)}. Você está no peso normal.";
-      } else if (imc <= 29.9) {
-        resultado =
-            "$nome, seu IMC é ${imc.toStringAsFixed(2)}. Você está na pré-obesidade.";
-      } else if (imc <= 34.9) {
-        resultado =
-            "$nome, seu IMC é ${imc.toStringAsFixed(2)}. Você está na obesidade nível 1.";
-      } else if (imc <= 39.9) {
-        resultado =
-            "$nome, seu IMC é ${imc.toStringAsFixed(2)}. Você está na obesidade nível 2.";
-      } else {
-        resultado =
-            "$nome, seu IMC é ${imc.toStringAsFixed(2)}. Você está na obesidade nível 3.";
-      }
-    });
-
+  void alert(BuildContext context, String msg) {
     showDialog(
       context: context,
-      builder: (context) {
+      builder: (BuildContext context) {
         return AlertDialog(
-          title: const Text("Diagnóstico"),
-          content: Text(resultado),
+          title: Text("Mensagens"),
+          content: Text(msg),
           actions: [
             ElevatedButton(
               onPressed: () {
                 Navigator.of(context).pop();
               },
-              child: const Text("OK"),
+              child: Text("Ok"),
             ),
           ],
         );
@@ -64,45 +45,46 @@ class _MainAppState extends State<MainApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
+            spacing: 20.0,
             children: [
-              const Text(
-                'Índice de massa corporal',
+              Text(
+                "Índice de Massa Corpórea",
                 style: TextStyle(
-                  color: Color.fromARGB(255, 0, 205, 208),
-                  fontWeight: FontWeight.bold,
-                  fontSize: 22.0,
+                  fontSize: 20.0,
+                  color: Color.fromARGB(255, 100, 0, 0),
                 ),
               ),
               TextField(
-                decoration: const InputDecoration(labelText: 'Nome'),
+                decoration: InputDecoration(labelText: "Nome"),
                 onChanged: (value) {
                   nome = value;
                 },
               ),
               TextField(
-                decoration: const InputDecoration(labelText: 'Peso'),
-                keyboardType: const TextInputType.numberWithOptions(),
+                decoration: InputDecoration(labelText: "Peso"),
                 onChanged: (value) {
-                  peso = double.tryParse(value) ?? 0.0;
+                  peso = double.parse(value);
                 },
               ),
               TextField(
-                decoration: const InputDecoration(labelText: 'Altura'),
-                keyboardType: const TextInputType.numberWithOptions(),
+                decoration: InputDecoration(labelText: "Altura"),
                 onChanged: (value) {
-                  altura = double.tryParse(value) ?? 0.0;
+                  altura = double.parse(value);
                 },
               ),
               ElevatedButton(
-                onPressed: processar,
-                child: const Text("Calcular"),
+                onPressed: () {
+                  calcular();
+                  alert(context, "$resultado");
+                },
+                child: Text("Calcular"),
               ),
-              Text(resultado),
             ],
           ),
         ),

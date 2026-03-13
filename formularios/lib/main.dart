1885376abab2f80c8d08a238754dc6b4473c6d8c
EnzoToniato567 <enzotoniato0303@gmail.com>
2026-03-13 07:41:04 -0300

    IMC dart.main

--- formularios/lib/main.dart
+++ formularios/lib/main.dart
@@ -20,7 +20,24 @@ class _AppState extends State<App> {
 
   void calcular() {
     imc = peso / (altura * altura);
-    resultado = "$nome seu IMC é ${imc.toStringAsFixed(1)}";
+
+    String classificacao;
+    if (imc < 18.5) {
+      classificacao = "Magreza";
+    } else if (imc < 24.9) {
+      classificacao = "Normal";
+    } else if (imc < 29.9) {
+      classificacao = "Sobrepeso";
+    } else if (imc < 34.9) {
+      classificacao = "Obesidade grau I";
+    } else if (imc < 39.9) {
+      classificacao = "Obesidade grau II";
+    } else {
+      classificacao = "Obesidade grau III";
+    }
+
+    resultado =
+        "$nome seu IMC é ${imc.toStringAsFixed(1)}. Classificação: $classificacao";
   }
 
   void alert(BuildContext context, String msg) {
